5d711d0c5a51543bc17f3a27f64e6e3d2c7935a3
TheSkout <111308237+TheSkout@users.noreply.github.com>
2023-03-13 16:02:31 +0300

    Initial commit

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ settings = {
 	'NAME BOT': 'PUT YOUR BOT NAME HERE', # BOT NAME
 	'ID': 'PUT YOUR BOT ID HERE', # BOT ID
 	'PREFIX': ';', # PREFIX
-	'OWNER':'PUT YOUR BOT OWNER USERNAME' ',    # OWNER USERNAME
+	'OWNER':'PUT YOUR BOT OWNER USERNAME',    # OWNER USERNAME
 	'OWNER NAME':'PUT YOUR BOT OWNER NAME' # OWNER NAME
 }
+# If you need any help with, you can open issue on GitHub, or you can DM me in Discord: TheSkout#8213
